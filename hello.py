323ad2e431ddc2e335bc1f0eacca86d076f6aa27
Khuzaifa Baseer <232061@students.au.edu.pk>
2026-09-25 14:47:22 +0500

    merged

--- hello.py
+++ hello.py
@@ -4,11 +4,8 @@ print("Hello", name)
 
 print("This line was added directly on GitHub.")
 
-print("This line was added directly on GitHub.")
-
 
 print("This line was added locally.")
-print("This line was added directly on GitHub.")
 
 
-print("This line was added locally.")
+
